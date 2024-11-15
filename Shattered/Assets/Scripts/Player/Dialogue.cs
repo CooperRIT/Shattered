2561aa1cf1093495458a1ea0270944d581d7a1c7
Zachary Karlin <zak1673@g.rit.edu>
2024-11-14 22:19:57 -0500

    Okay one last quick dialogue thing

--- Shattered/Assets/Scripts/Player/Dialogue.cs
+++ Shattered/Assets/Scripts/Player/Dialogue.cs
@@ -17,28 +17,28 @@ public class Dialogue : MonoBehaviour
     public void OnFirstWave()
     {
         dialoguePanel.SetActive(true);
-        dialogueBox.text = "The devils that were called of the Northern Empire assailed friend and fo alike.";
+        dialogueBox.text = "Kelder: The devils that were called of the Northern Empire assailed friend and fo alike.";
         Invoke("ClearDialogueBox", 5);
     }
 
     public void OnFirstAttack()
     {
         dialoguePanel.SetActive(true);
-        dialogueBox.text = "I cast... MAGIC!";
+        dialogueBox.text = "Kelder: I cast... MAGIC!";
         Invoke("ClearDialogueBox", 2);
     }
 
     public void OnFirstDpsTower()
     {
         dialoguePanel.SetActive(true);
-        dialogueBox.text = "The Northern Empire unlashede the hordes of Hell using... Demon-ology.";
+        dialogueBox.text = "Kelder: The Northern Empire unlashede the hordes of Hell using... Demon-ology.";
         Invoke("ClearDialogueBox", 5);
     }
 
     public void OnFirstSupportTower()
     {
         dialoguePanel.SetActive(true);
-        dialogueBox.text = "The Holy Kingdom of Ethshar called on the gods to push back the devils.";
+        dialogueBox.text = "Kelder: The Holy Kingdom of Ethshar called on the gods to push back the devils.";
         Invoke("ClearDialogueBox", 5);
     }
 }
